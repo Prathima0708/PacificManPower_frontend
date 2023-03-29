0f6e439547c3d807d177c30d3f5f3f6aa2da63b2
Prathima0708 <kotianpriya64@gmail.com>
2023-03-29 16:16:01 +0530

    initial commit

---
+++ src/pages/Home/Layout1/Layout1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Section from "../Layout1/Section";
+import Home from "../Home";
+import MetaTags from "react-meta-tags";
+
+const Layout1 = () => {
+  return (
+    <div>
+      <MetaTags>
+        <title>PacificManpower</title>
+        <script src="https://unicons.iconscout.com/release/v3.0.2/script/monochrome/bundle.js"></script>
+      </MetaTags>
+      <Section />
+      <Home />
+    </div>
+  );
+};
+
+export default Layout1;
